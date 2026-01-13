e8b41dffab86d43a16fde025fd64fc02babbbdc5
Bart Ozdoba <32847561+bart-oz@users.noreply.github.com>
2026-01-13 19:05:48 +0100

    [SO-003][FEAT] Set up Rails Engine with initialization hooks (#25)
    SolidObserver into Rails applications with safety checks and route mounting.
    
    Key changes:
    - Created Engine class with isolated namespace
    - Added check_solid_queue_availability class method to warn if SolidQueue missing
    - Added check_and_activate_subscribers method with table existence checks
    - Implemented conditional route mounting based on ui_enabled config
    - Added Rails initializers with deferred activation until after initialization
    - Graceful error handling for ActiveRecord::NoDatabaseError

--- lib/solid_observer.rb
+++ lib/solid_observer.rb
@@ -2,6 +2,7 @@
 
 require_relative "solid_observer/version"
 require_relative "solid_observer/configuration"
+require_relative "solid_observer/engine" if defined?(Rails::Engine)
 
 module SolidObserver
   class Error < StandardError; end
---
+++ spec/solid_observer/engine_spec.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe SolidObserver::Engine do
+  let(:logger) { instance_double(Logger, warn: nil, info: nil) }
+
+  before do
+    allow(Rails).to receive(:logger).and_return(logger)
+  end
+
+  it "is a Rails::Engine" do
+    expect(described_class.ancestors).to include(Rails::Engine)
+  end
+
+  it "isolates namespace to SolidObserver" do
+    expect(described_class.isolated?).to be true
+  end
+
+  describe ".check_solid_queue_availability" do
+    it "warns when SolidQueue is not defined" do
+      hide_const("SolidQueue")
+
+      expect(logger).to receive(:warn).with(/SolidQueue not detected/)
+
+      described_class.check_solid_queue_availability
+    end
+
+    it "does not warn when SolidQueue is defined" do
+      stub_const("SolidQueue", Class.new)
+
+      expect(logger).not_to receive(:warn)
+
+      described_class.check_solid_queue_availability
+    end
+  end
+
+  describe ".check_and_activate_subscribers" do
+    let(:connection) { instance_double(ActiveRecord::ConnectionAdapters::AbstractAdapter) }
+
+    before do
+      allow(ActiveRecord::Base).to receive(:connection).and_return(connection)
+    end
+
+    it "logs activation message when tables exist" do
+      allow(connection).to receive(:table_exists?).with("solid_observer_queue_events").and_return(true)
+
+      expect(logger).to receive(:info).with(/Tables detected/)
+
+      described_class.check_and_activate_subscribers
+    end
+
+    it "logs migration instruction when tables do not exist" do
+      allow(connection).to receive(:table_exists?).with("solid_observer_queue_events").and_return(false)
+
+      expect(logger).to receive(:info).with(/Tables not found/)
+
+      described_class.check_and_activate_subscribers
+    end
+
+    it "rescues gracefully when database is not ready" do
+      allow(connection).to receive(:table_exists?).and_raise(ActiveRecord::NoDatabaseError)
+
+      expect(logger).to receive(:info).with(/Database not ready/)
+
+      described_class.check_and_activate_subscribers
+    end
+  end
+
+  describe "routes" do
+    it "has a route set" do
+      expect(described_class.routes).to be_a(ActionDispatch::Routing::RouteSet)
+    end
+  end
+
+  describe "initializers" do
+    it "defines solid_observer.check_tables initializer" do
+      initializer_names = described_class.initializers.map(&:name)
+      expect(initializer_names).to include("solid_observer.check_tables")
+    end
+  end
+end
